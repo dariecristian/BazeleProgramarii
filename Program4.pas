310a4fdcdc1bb13a73f5c72be98abedcba67f15e
dariecristian <71259662+dariecristian@users.noreply.github.com>
2020-09-30 14:44:19 +0300

    Add files via upload

---
+++ Program4.pas
@@ -0,0 +1,12 @@
+﻿{Exercițiul 8}
+var
+v1, v2, s, t:real;
+begin
+v1:=1; 
+v2:=2;
+writeln('Viteza fetelor este ', (v1+v2), ' km/h');
+writeln('Distanta este: ');
+readln(s);
+t:=s/(v1+v2);
+writeln ('Timpul in care au ajuns fetele : ',t ,' ore');
+end. 
