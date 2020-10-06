b8ff646e8b2ac31c44799ae964e1408d9d047445
dariecristian <71259662+dariecristian@users.noreply.github.com>
2020-10-06 16:11:25 +0300

    Add files via upload

---
+++ ProgramEx16.pas
@@ -0,0 +1,15 @@
+﻿var 
+N, M, O, a, b, c, d:Integer;
+begin
+writeln('Introdu ora lectiei: ');
+readln(N,M);
+a:=385 div 60;
+b:=385 mod 60;
+if b+M > 60 then 
+begin
+  c:=(b+M)-60;
+  N:=N+1;
+end else c:=b+M;
+O:=N+a;
+writeln('A 7-tea lectie se termina la ', O, ':', c);
+end.
