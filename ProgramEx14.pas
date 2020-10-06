8c25968a2603214fc084fd22eeaeb6d3ace60e1a
dariecristian <71259662+dariecristian@users.noreply.github.com>
2020-10-06 15:38:55 +0300

    Add files via upload

---
+++ ProgramEx14.pas
@@ -0,0 +1,12 @@
+﻿var
+N, S, D:integer;
+begin
+writeln('Introdu D: ');
+readln(D);
+writeln ('Automobilul coasta ' , D, ' euro');
+writeln('Introdu costul automibului vechi:');
+readln (N);
+writeln('Introdu suma pe contul bancar:');
+readln(S);
+writeln('Suma ramasa va fi: ', (N+S)-D,' euro:');
+end.
