b7e5c02ced5d854f6b873909e7f18cb0fcb7cd15
dariecristian <71259662+dariecristian@users.noreply.github.com>
2020-10-07 13:23:45 +0300

    Add files via upload

---
+++ ProgramEx1.pas
@@ -0,0 +1,22 @@
+﻿var
+a,b,c,max,min:integer;
+begin
+read(a);
+read(b);
+read(c);
+  if (a > b) and (a > c) then
+    max:= a
+      else
+  if (b > a) and (b > c) then
+    max:= b
+      else
+    max:= c;
+  if (b < a) and (b < c) then
+    min := b
+      else
+  if (a < b) and (a < c) then
+    min := a
+      else
+    min := c;
+  writeln('Diferenta este : ', max-min,'');    
+end.
