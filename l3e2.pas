816591a1aa843e91559f71cd203781b0ba711a72
dariecristian <71259662+dariecristian@users.noreply.github.com>
2020-10-21 13:29:51 +0300

    Add files via upload

---
+++ l3e2.pas
@@ -0,0 +1,12 @@
+﻿var
+a:integer;
+begin
+writeln('Introdu A');
+readln(a);
+case a of
+1..10:writeln('decada I');
+11..20:writeln('decada II');
+21..30:writeln('decada III');
+31:writeln('decada IV');
+end;
+end.
---
+++ l3e5.pas
@@ -0,0 +1,17 @@
+﻿var
+n:integer;
+begin
+writeln('Introdu cifra');
+readln(n);
+case n of
+1:writeln('unu');
+2:writeln('doi');
+3:writeln('trei');
+4:writeln('patru');
+5:writeln('cinci');
+6:writeln('șase');
+7:writeln('șapte');
+8:writeln('opt');
+9:writeln('nouă');
+end;
+end.
