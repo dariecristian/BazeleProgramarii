51b0d2be4ad34d38d35a98a81a1eea9a39578d32
dariecristian <71259662+dariecristian@users.noreply.github.com>
2020-12-11 15:08:58 +0200

    Add files via upload

---
+++ L5E3.pas
@@ -0,0 +1,9 @@
+﻿var a,b,c,d,e,f,i:integer;
+begin
+  repeat
+ readln(a,b);
+ if a = 1 then writeln(a,' ',b,' sunt prime');
+ if b = 1 then writeln(a,' ',b,' sunt prime');
+   c:=a*b;
+  until c=0;
+end.
---
+++ L8E3.pas
@@ -0,0 +1,11 @@
+﻿type vector = array [1..100] of integer;
+var a:vector;
+i,k,j,b:integer;
+begin
+  readln(k);
+  for i:=1 to k do readln(a[i]);
+  j:=a[k];
+  a[k]:=a[1];
+  a[1]:=j;
+  for i:=1 to k do writeln(a[i]);
+end.
